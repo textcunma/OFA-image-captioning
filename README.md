0bc4eedb0c9af656fd729347b43f3b2d2b4f9b0e
test <test@exsample.com>
2023-12-16 10:45:03 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # OFA-image-captioning
 image captioning by OFA
+
+
+## 準備
+
+### 仮想環境
+
+```
+conda env create -n ofa -f ofa.yml
+```
+
+### インストール
+モデルのインストール
+
+OFA-hugeの場合
+```
+sudo apt-get install git-lfs
+git lfs clone https://huggingface.co/OFA-Sys/OFA-huge
+```
+
+その他のサイズは以下を参照
+https://huggingface.co/OFA-Sys
+
+## 参考
+
+OFA-huge
+https://huggingface.co/OFA-Sys/ofa-huge
+
---
+++ main.py
@@ -0,0 +1,47 @@
+# Ref: https://huggingface.co/OFA-Sys/ofa-large
+
+from PIL import Image
+from torchvision import transforms
+import torch
+from transformers import OFATokenizer, OFAModel
+from OFA.transformers.src.transformers.models.ofa.generate import sequence_generator
+
+def image_caption(image_path, ckpt_dir = "./OFA-huge"):
+
+    mean, std = [0.5, 0.5, 0.5], [0.5, 0.5, 0.5]
+    resolution = 480
+    patch_resize_transform = transforms.Compose([
+            lambda image: image.convert("RGB"),
+            transforms.Resize((resolution, resolution), interpolation=Image.BICUBIC),
+            transforms.ToTensor(), 
+            transforms.Normalize(mean=mean, std=std)
+        ])
+
+    tokenizer = OFATokenizer.from_pretrained(ckpt_dir)
+
+    txt = " what does the image describe?"
+    inputs = tokenizer([txt], return_tensors="pt").input_ids
+    img = Image.open(image_path)
+    patch_img = patch_resize_transform(img).unsqueeze(0)
+
+    # using the generator of fairseq version
+    model = OFAModel.from_pretrained(ckpt_dir, use_cache=True)
+    generator = sequence_generator.SequenceGenerator(
+                        tokenizer=tokenizer,
+                        beam_size=5,
+                        max_len_b=16, 
+                        min_len=0,
+                        no_repeat_ngram_size=3,
+                    )
+    data = {}
+    data["net_input"] = {"input_ids": inputs, 'patch_images': patch_img, 'patch_masks':torch.tensor([True])}
+    gen_output = generator.generate([model], data)
+    
+    gen = [gen_output[i][0]["tokens"] for i in range(len(gen_output))]
+
+    return tokenizer.batch_decode(gen, skip_special_tokens=True)
+
+
+# 実行
+result_txt = image_caption("test.png")
+print(result_txt)
